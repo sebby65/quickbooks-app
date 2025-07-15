ff5874bad82345b33650dcd0b89381c0610e892f
sebby65 <sebwolterzz@gmail.com>
2025-07-15 11:13:53 -0500

    fetch_qb_data.py

--- fetch_qb_data.py
+++ fetch_qb_data.py
@@ -1,17 +1,14 @@
 import requests
+import pandas as pd
 
 def fetch_qb_data(auth_client, realm_id):
     access_token = auth_client.access_token
-    url = f"https://quickbooks.api.intuit.com/v3/company/{realm_id}/reports/ProfitAndLoss"
-
     headers = {
         "Authorization": f"Bearer {access_token}",
         "Accept": "application/json",
+        "Content-Type": "application/json"
     }
+    url = f"https://quickbooks.api.intuit.com/v3/company/{realm_id}/reports/ProfitAndLoss?date_macro=LastYear"
 
     response = requests.get(url, headers=headers)
-
-    if response.status_code != 200:
-        raise Exception(f"Failed to fetch P&L data: {response.status_code}, {response.text}")
-
     return response.json()
